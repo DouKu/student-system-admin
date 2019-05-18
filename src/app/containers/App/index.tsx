a05ab82e148acb8f231c647580627d640366eca3
lgybetter <437675103@qq.com>
2019-05-18 14:35:22 +0800

    :art: build

--- src/app/containers/App/index.tsx
+++ src/app/containers/App/index.tsx
@@ -11,16 +11,16 @@ import ContentHeader from '../../components/ContentHeader';
 import Footer from '../../components/Footer';
 
 // modules
-import BaseMsg from '../../containers/Modules/BaseMsg';
-import AssetsMsg from '../../containers/Modules/AssetsMsg';
-import Referrer from '../../containers/Modules/Referrer';
-import WebPerformance from '../../containers/Modules/WebPerformance';
-import System from '../../containers/Modules/System';
-import User from '../../containers/Modules/User';
+// import BaseMsg from '../../containers/Modules/BaseMsg';
+// import AssetsMsg from '../../containers/Modules/AssetsMsg';
+// import Referrer from '../../containers/Modules/Referrer';
+// import WebPerformance from '../../containers/Modules/WebPerformance';
+// import System from '../../containers/Modules/System';
+// import User from '../../containers/Modules/User';
 import Home from '../../containers/Modules/Home';
 import Student from '../../containers/Modules/Student';
-import Error from '../../containers/Modules/Error';
-import OverViewRealTime from '../../containers/Modules/RealTime/overView';
+// import Error from '../../containers/Modules/Error';
+// import OverViewRealTime from '../../containers/Modules/RealTime/overView';
 
 const { Content } = Layout;
 
@@ -63,14 +63,14 @@ class App extends React.Component<AppProps, AppState> {
               <Content style={{ margin: 16, padding: 16,  minHeight: 750, background: '#fff' }}>
                 <Route exact={true} path="/" component={Home}/>
                 <Route exact={true} path="/student" component={Student}/>
-                <Route exact={true} path="/base/webMsg" component={BaseMsg}/>
+                {/* <Route exact={true} path="/base/webMsg" component={BaseMsg}/>
                 <Route exact={true} path="/common/referrer" component={Referrer}/>
                 <Route exact={true} path="/common/system" component={System}/>
                 <Route exact={true} path="/common/user" component={User}/>
                 <Route exact={true} path="/performance/webMsg" component={WebPerformance}/>
                 <Route exact={true} path="/performance/assetsMsg" component={AssetsMsg}/>
                 <Route exact={true} path="/error" component={Error}/>
-                <Route exact={true} path="/realtime/webMsg" component={OverViewRealTime}/>
+                <Route exact={true} path="/realtime/webMsg" component={OverViewRealTime}/> */}
               </Content>
               <Footer/>
             </Content>
